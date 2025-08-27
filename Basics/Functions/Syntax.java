2a1e3cb3cf8f3b4c2ea1e01ff7132d013e7778e7
Musab Ur Rehman <rehmanmusab0302@gmail.com>
2025-08-27 11:59:55 +0500

    syntax for functions

---
+++ Basics/Functions/Syntax.java
@@ -0,0 +1,12 @@
+package Functions;
+
+public class Syntax {
+    public static void main(String[] args) {
+        /*
+        access_modifier return_type func_name () {
+        body
+        return
+        }
+        */
+    }
+}
