ad51f9fa3b8c76ad44196b3562c0803a5a4d1804
Musab Ur Rehman <rehmanmusab0302@gmail.com>
2025-09-02 10:43:10 +0500

    syntax of array

---
+++ Basics/ArraysAndLists/Main.java
@@ -0,0 +1,10 @@
+package ArraysAndLists;
+
+public class Main {
+    public static void main(String[] args) {
+        //syntax
+        //datatype[] name = new datatype[size];
+        int arr[] = new int[5];
+        System.out.println(arr[4]);
+    }
+}
